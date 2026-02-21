4d6b1391e1f4951bdec70af1d147c8be839833bc
SamMie341 <soukthavixay2018@gmail.com>
2026-02-22 00:39:30 +0700

    test3

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,42 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { json, urlencoded } from 'express';
 
+let cachedServer: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(json({limit: '500mb'}));
-  app.use(urlencoded({extended: true, limit: '500mb'}))
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+
+  app.use(json({ limit: '500mb' }));
+  app.use(urlencoded({ extended: true, limit: '500mb' }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.enableCors();
+
   const config = new DocumentBuilder()
-  .setTitle('E-Learning API')
-  .setDescription('API documentation for E-Learning Platform')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('E-Learning API')
+    .setDescription('API documentation for E-Learning Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
-  
-  // await app.listen(process.env.PORT ?? 8000);
+  await app.init();
+
+  // return serverless(app.getHttpAdapter().getInstance());
 }
-bootstrap();
+
+export default async function handler(req, res) {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  return cachedServer(req, res);
+}
